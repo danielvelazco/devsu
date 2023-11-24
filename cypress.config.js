9dfecdd1f55d2723866fd056dc985f644cf7dfba
Daneil Velazco <danvelazco27@gmail.com>
2023-11-24 15:56:18 -0500

    add video support

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: "cypress/tests/**/*.cy.js"
+    specPattern: "cypress/tests/**/*.cy.js",
+    video: true,
   },
 });
